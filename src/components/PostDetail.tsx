978ac106d71ddb7313e12323486c5c308bceeedf
aryabenade <benadearya@gmail.com>
2025-10-30 12:05:10 +0530

    add vite base url

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -40,7 +40,7 @@ export const PostDetail = ({ postId }: Props) => {
 
     return (
         <div className="space-y-6">
-      <h2 className="text-6xl font-bold mb-6 text-center bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
+      <h2 className="text-6xl font-bold mb-6 text-center bg-linear-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
         {data?.title}
       </h2>
       {data?.image_url && (
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const PostItem = ({ post }: Props) => {
     return (
         <div className="relative group">
-            <div className="absolute -inset-1 rounded-[20px] bg-gradient-to-r from-pink-600 to-purple-600 blur-sm opacity-0 group-hover:opacity-50 transition duration-300 pointer-events-none"></div>
+            <div className="absolute -inset-1 rounded-[20px] bg-linear-to-r from-pink-600 to-purple-600 blur-sm opacity-0 group-hover:opacity-50 transition duration-300 pointer-events-none"></div>
             <Link to={`/post/${post.id}`} className="block relative z-10">
                 <div className="w-80 h-76 bg-[rgb(24,27,32)] border border-[rgb(84,90,106)] rounded-[20px] text-white flex flex-col p-5 overflow-hidden transition-colors duration-300 group-hover:bg-gray-800">
                     {/* Header: Avatar and Title */}
@@ -20,7 +20,7 @@ export const PostItem = ({ post }: Props) => {
                                 className="w-[35px] h-[35px] rounded-full object-cover"
                             />
                         ) : (
-                            <div className="w-[35px] h-[35px] rounded-full bg-gradient-to-tl from-[#8A2BE2] to-[#491F70]" />
+                            <div className="w-[35px] h-[35px] rounded-full bg-linear-to-tl from-[#8A2BE2] to-[#491F70]" />
                         )}
                         <div className="flex flex-col flex-1">
                             <div className="text-[20px] leading-[22px] font-semibold mt-2">
