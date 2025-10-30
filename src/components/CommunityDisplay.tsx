376966cf13f645d2fcc5f5ac6ba76d8f48bde1bd
aryabenade <benadearya@gmail.com>
2025-10-30 12:28:28 +0530

    community error fixed in community display

--- src/components/CommunityDisplay.tsx
+++ src/components/CommunityDisplay.tsx
@@ -43,7 +43,7 @@ export const CommunityDisplay = ({ communityId }: Props) => {
     return (
         <div>
             <h2 className="text-6xl font-bold mb-6 text-center bg-linear-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
-                {data && data[0].communities.name} Community Posts
+                {data && data[0]?.communities.name} Community Posts
             </h2>
 
             {data && data.length > 0 ? (
